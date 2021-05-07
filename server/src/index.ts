cbd1514409b2b7eb8ceda3c07ac59812b83b78a5
Janitha Tennakoon <janitha000@gmail.com>
2021-05-07 09:53:15 +0800

    init

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
+
+const main = async () => {
+    const app = express();
+
+
+};
+
+main().catch((err) => {
+    console.log(err);
+});
